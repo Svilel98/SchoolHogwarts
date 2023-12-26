008ad2c18bb0cd5ce229fd01d6240386dcc055bf
Иван <ivanhlop@gmail.com>
2023-12-26 23:15:33 +0300

    Тесты WebMvcTest

--- src/test/java/ru/hogwarts/school/TestFaculty.java
+++ src/test/java/ru/hogwarts/school/TestFaculty.java
@@ -1,23 +1,19 @@
 package ru.hogwarts.school;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.hogwarts.school.contoller.FacultyController;
 import ru.hogwarts.school.contoller.StudentController;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.FacultyRepository;
@@ -34,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-class SchoolApplicationTests {
+public class TestFaculty {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
@@ -52,58 +48,57 @@ class SchoolApplicationTests {
     @Autowired
     private ObjectMapper objectMapper;
     @InjectMocks
-    private StudentController studentController;
+    private FacultyController facultyController;
 
     @Test
-    public void saveStudentTest() throws Exception {
+    public void saveFacultyTest() throws Exception {
         final String name = "Andrey";
-        final Integer age = 24;
+        final String color = "Зеленый";
         final long id = 1;
-        JSONObject studentObject = new JSONObject();
-        studentObject.put("name", name);
-        studentObject.put("age", age);
-        Student testStudent = new Student();
-        testStudent.setId(id);
-        testStudent.setName(name);
-        testStudent.setAge(age);
-        when(studentRepository.save(any(Student.class))).thenReturn(testStudent);
-        when(studentRepository.findById(any(Long.class))).thenReturn(Optional.of(testStudent));
+        JSONObject facultyObject = new JSONObject();
+        facultyObject.put("color", color);
+        facultyObject.put("name", name);
+        Faculty testFaculty = new Faculty();
+        testFaculty.setId(id);
+        testFaculty.setColor(color);
+        testFaculty.setName(name);
+        when(facultyRepository.save(any(Faculty.class))).thenReturn(testFaculty);
+        when(facultyRepository.findById(any(Long.class))).thenReturn(Optional.of(testFaculty));
         mockMvc.perform(MockMvcRequestBuilders
-                        .post("/student")
-                        .content(studentObject.toString())
+                        .post("/faculty")
+                        .content(facultyObject.toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
                 .andExpect(jsonPath("$.name").value(name))
-                .andExpect(jsonPath("$.age").value(age));
+                .andExpect(jsonPath("$.color").value(color));
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/student/" + id)
+                        .get("/faculty/" + id)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
                 .andExpect(jsonPath("$.name").value(name))
-                .andExpect(jsonPath("$.age").value(age));
+                .andExpect(jsonPath("$.color").value(color));
         mockMvc.perform(MockMvcRequestBuilders
-                        .put("/student")
-                        .content(objectMapper.writeValueAsString(testStudent))
+                        .put("/faculty")
+                        .content(objectMapper.writeValueAsString(testFaculty))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
                 .andExpect(jsonPath("$.name").value(name))
-                .andExpect(jsonPath("$.age").value(age));
+                .andExpect(jsonPath("$.color").value(color));
         mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/student/" + id)
-                        .content(objectMapper.writeValueAsString(testStudent))
+                        .delete("/faculty/" + id)
+                        .content(objectMapper.writeValueAsString(testFaculty))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/student/")
-                        .queryParam("min", "10")
-                        .queryParam("max", "30")
-                        .content(objectMapper.writeValueAsString(testStudent))
+                        .get("/faculty")
+                        .queryParam("color", "Зеленый")
+                        .queryParam("name", "Andrey")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
